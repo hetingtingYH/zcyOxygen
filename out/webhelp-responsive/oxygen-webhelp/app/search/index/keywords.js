93aca24d0d174215429c7ff5990c997f707400ed
咏荷 <@LAPTOP-1HOS180V>
2023-12-26 11:05:30 +0800

    删除多余文件，新增两个主题

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++
@@ -1,9 +0,0 @@
-var keywords=[{w:"\u7F51\u4E0A\u8D85\u5E02\u5B9A\u4E49",p:["p0"]},{w:"\u9002\u7528\u8303\u56F4",p:["p1"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]}];
-var ph={};
-ph["p0"]=[0];
-ph["p1"]=[1];
-ph["p2"]=[2, 3, 4];
-var keywordsInfo = {
-    keywords: keywords,
-    ph: ph
-}
